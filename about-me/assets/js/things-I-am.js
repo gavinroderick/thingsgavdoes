ece6230d807abbed896356b49662b856348a8ef6
Gavin Roderick <gavin@intelligentgrowthsolutions.com>
2022-05-28 15:49:41 +0100

    feat(about-me): create initial home page

---
+++ about-me/assets/js/things-I-am.js
@@ -0,0 +1,28 @@
+const things = [
+  "Software Engineer",
+  "very amatuer woodworker",
+  "former bartender",
+  "converted coffee snob",
+  "pushover to a spoiled resue cat",
+  "parnter to an amazing woman",
+];
+
+function randomizeListOrder() {
+  for (i = things.length - 1; i > 0; i--) {
+    j = Math.floor(Math.random() * i);
+    k = things[i];
+    things[i] = things[j];
+    things[j] = k;
+  }
+}
+
+let count = 0;
+
+function cycleArray() {
+  let index = count % things.length;
+  document.getElementById("what-am-i").innerHTML = things[index];
+  count++;
+}
+
+randomizeListOrder();
+setInterval(cycleArray, 10000);
